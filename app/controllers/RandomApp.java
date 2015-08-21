1e0714b50009098f0cda49644df64b4095c41687
hjgwon <hjkwon@nextree.co.kr>
2015-08-21 16:06:22 +0900

    1차 고도화

--- app/controllers/RandomApp.java
+++ app/controllers/RandomApp.java
@@ -55,16 +55,20 @@ public class RandomApp extends Controller {
 		while(true){
 		    int lane = ((int)(Math.random() * Integer.parseInt(laneSize)))  + 1;
 		    
+		    if(Integer.parseInt(laneSize) <= Random.findCountBySequenceAndSeed(sequence, seed)){
+		    	return badRequest("레인이 모두 배정되었습니다.");
+		    }
+		    
 		    if(Random.findByValues(sequence, seed, String.valueOf(lane)) == null) {
 		    	
-		    	if(Integer.parseInt(laneSize) <= Random.findCountBySequenceAndSeed(sequence, seed)){
-			    	return badRequest("레인이 모두 배정되었습니다.");
-			    }
-		    	
 		    	String id = sequence + "_" + seed + "_" + String.valueOf(lane);
-				new Random(id, sequence, name, String.valueOf(lane), seed).insert();
-				break; 
-				
+		    	
+		    	try{
+		    		new Random(id, sequence, name, String.valueOf(lane), seed).insert();
+		    		break; 
+		    	} catch(RuntimeException e){
+		    		e.printStackTrace();
+		    	}
 		    }
 		}
 		 
@@ -107,25 +111,54 @@ public class RandomApp extends Controller {
 			
 		    int newLane = ((int)(Math.random() * Integer.parseInt(laneSize)))  + 1;
 		    
+		    if(Integer.parseInt(laneSize) <= Random.findCountBySequenceAndSeed(sequence, seed)){
+		    	return badRequest("레인이 모두 배정되었습니다.");
+		    }
+		    
 		    Random random = Random.findByValues(sequence, seed, String.valueOf(newLane));
 		    
 		    if(random == null) {
-		    	
-		    	if(Integer.parseInt(laneSize) <= Random.findCountBySequenceAndSeed(sequence, seed)){
-			    	return badRequest("레인이 모두 배정되었습니다.");
-			    }
-		    	
-		    	oldRandom.setSeed(seed);
-		    	oldRandom.setLane(String.valueOf(newLane));
-		    	oldRandom.setName(name);
-		    	
-		    	oldRandom.update();
-		    	
-				break; 
+		    	try{
+			    	oldRandom.setSeed(seed);
+			    	oldRandom.setLane(String.valueOf(newLane));
+			    	oldRandom.setName(name);
+			    	
+			    	oldRandom.update();
+			    	break;
+			    	
+		    	}catch(RuntimeException e){
+		    		e.printStackTrace();
+		    	} 
 		    }
 		}
 		
     	return redirect(routes.RandomApp.indexAdmin(sequence, laneSize));
     }
+    
+    @Transactional
+    public Result delete() {
+    	//
+    	DynamicForm requestData = Form.form().bindFromRequest();
+		String seed = requestData.get("removeSeed");
+		String sequence = requestData.get("removeSequence");
+		String lane = requestData.get("removeLane");
+		String laneSize = requestData.get("laneSize");
+		
+		if(StringUtils.isNullOrEmpty(seed)) {
+			return badRequest("delete 시드를 선택하세요.");
+		}
+		
+		if(StringUtils.isNullOrEmpty(lane)) {
+			return badRequest("레인을 입력하세요.");
+		}
+		
+		Random oldRandom = Random.findByValues(sequence, seed, String.valueOf(lane));
+		if(oldRandom == null) {
+			return badRequest("입력된 정보가 없습니다.");
+	    }
+		
+		oldRandom.delete();
 
+    	return redirect(routes.RandomApp.indexAdmin(sequence, laneSize));
+    }
 }
